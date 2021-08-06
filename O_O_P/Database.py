3456028e3b5fec66336dd34577f7541da43f895a
Hasan <hasanozdemir1@trakya.edu.tr>
2021-08-06 23:04:43 +0300

    CRUD operations with PostreSQL. READ DONE CREAT DONE

--- O_O_P/Database.py
+++ O_O_P/Database.py
@@ -1,6 +1,7 @@
 import psycopg2
 
 class Database:
+
     def __init__(self, host, db_name, user, password):
         self._host=host
         self._db_name = db_name
@@ -24,10 +25,22 @@ class Database:
         cursor.execute(query)
         for row in cursor.fetchall():
             print(row)
+    def insert_db(self,email,full_name,username,password):
+        """
 
-
+        :param email:
+        :param full_name:
+        :param username:
+        :param password:
+        :return: log message about process result (inserted, failed)
+        """
+        cursor = self._connection.cursor()
+        sql_query="INSERT INTO public.users(email,full_name,username,password) VALUES(%s,%s,%s,%s);"
+        cursor.execute(sql_query ,(email,full_name,username,password))
+        self._connection.commit()
+        print("Successfully inserted")
+        cursor.close()
 
 
 db=Database('localhost','User','postgres','Hsn58.34')
 db.read_db()
-
