5a4797349140c4d993bca906f9f5180fb66fd9ed
Hasan <hasanozdemir1@trakya.edu.tr>
2021-09-04 23:46:59 +0300

    fast_api_practice added

---
+++ api_practices/fast_api_practice.py
@@ -0,0 +1,20 @@
+# Further redhttps://fastapi.tiangolo.com/
+from fastapi import FastAPI
+
+app = FastAPI()
+
+dummy_data = [i for i in range(100)]
+
+@app.get("/")
+def read_root():
+    return {"hello": "world"}
+
+@app.get("/course/{course_id}")
+def my_course(course_id:int):
+    return {"course_id":course_id}
+
+
+
+@app.get("/my/page/items/")
+async def read_item(page: int = 0, limit: int = 0, skip: int = 1):
+    return dummy_data[page*10: page*10 + limit: skip]
