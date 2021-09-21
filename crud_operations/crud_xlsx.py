7191bfd9ccd828719b195a8979d96851532092ff
Hasan <hasanozdemir1@trakya.edu.tr>
2021-09-21 18:57:54 +0300

    Read and write method added/updated to XlsxOperations class.

--- crud_operations/crud_xlsx.py
+++ crud_operations/crud_xlsx.py
@@ -2,9 +2,7 @@
 from pathlib import Path
 import openpyxl as oxl
 
-
 file=Path('data_sources','VOCABULARY.xlsx')
-
 
 class XlsxOperations:
 
@@ -14,6 +12,10 @@ class XlsxOperations:
         self._sheet_obj=self._workbook_obj.active
 
     def read_all_xlsx(self)->list:
+        """
+        Read all data from sheet
+        :return result_df:<list:str> after read data to process return as list
+        """
         max_row=self._sheet_obj.max_row
         result_df:list=[]
         for item in range(1,max_row+1):
@@ -23,6 +25,20 @@ class XlsxOperations:
         result_df=list(set(result_df))
         return result_df
 
+    def write_all_xlsx(self,data:list,new_sheet_name:str) ->None:
+        """
+        This method create and excel sheet with given sheet name and write given data to a new sheet
+        :param data:<list:str> list of data
+        :param new_sheet_name:<str> sheet name for creating new
+        :return:
+        """
+        workbook=oxl.Workbook()
+        worksheet=workbook.active
+        for row,item in enumerate(data):
+            col_cell='A'
+            worksheet[col_cell+str(row+1)]=str(item)
+        workbook.save(f'{new_sheet_name}.xlsx')
+
+
 xlsx_obj=XlsxOperations(file)
 result:list=xlsx_obj.read_all_xlsx()
-print(len(result))
