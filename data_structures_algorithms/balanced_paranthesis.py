fe2febecafe87a38d1e0e99cecbaa3947e25b5e7
Hasan <hasanozdemir1@trakya.edu.tr>
2021-10-25 01:23:42 +0300

    Algorithm check paranthesis matching

---
+++ data_structures_algorithms/balanced_paranthesis.py
@@ -0,0 +1,15 @@
+# is a string balanced
+# balanced -> {} squiqles , [] brackets, () parenthesis
+
+def is_balanced(text:str)->bool:
+    punctuation=[]
+    tokens=['[',']','(',')','{','}']
+    for i in text:
+        if i in tokens:
+            punctuation.append(i)
+    if (punctuation.count('(')==punctuation.count(')') and punctuation.count('[')==punctuation.count(']') and punctuation.count('{')==punctuation.count('}')):
+        return True
+    else:
+        return False
+
+print(is_balanced('('))
