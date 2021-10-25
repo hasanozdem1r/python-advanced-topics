955d00a8fec4075cf1bae9e2b0f80c431e726396
Hasan <hasanozdemir1@trakya.edu.tr>
2021-10-25 21:57:29 +0300

    Heap operations

---
+++ data_structures_algorithms/heapq_practices.py
@@ -0,0 +1,18 @@
+# heap rules
+# h[k] <= h[2*k + 1] and h[k] <= h[2*k + 2]
+# https://realpython.com/python-heapq-module/
+import heapq as hq
+
+# convert array to heap
+ar=[3,5,1,2,6,8,7]
+hq.heapify(ar)
+print(ar)
+
+# pop element
+hq.heappop(ar)
+print(ar)
+
+# push element
+hq.heappush(ar,4)
+print(ar)
+
