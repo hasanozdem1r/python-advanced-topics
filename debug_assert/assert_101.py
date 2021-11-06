ccd5804a96ac63720226dfd2293979e4bcddcbe6
Hasan <hasanozdemir1@trakya.edu.tr>
2021-11-07 00:24:07 +0300

    Assert method usage

---
+++ debug_assert/assert_101.py
@@ -0,0 +1,15 @@
+# Best use cases for assert method
+# Ref -> Python Tricks
+def apply_discount(product,discount):
+    price=int(product['price']*(1.0-discount))
+    # It will guarantee that, no matter what,
+    # discounted prices calculated by this function cannot be lower than 0
+    assert 0<=price<=product['price']
+    return price
+
+shoes = {'name': 'Fancy Shoes', 'price': 14900}
+
+price=apply_discount(shoes,1.25)
+print(price)
+
+
