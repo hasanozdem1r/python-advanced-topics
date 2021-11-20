6fdf658de5e0b439a4a2b067f0ae44c47a00923e
Hasan <hasanozdemir1@trakya.edu.tr>
2021-11-20 19:28:26 +0300

    One of the famous interview question

--- data_structures_algorithms/questions/qa1_balanced_paranthesis.py
+++ data_structures_algorithms/questions/qa1_balanced_paranthesis.py
@@ -1,27 +1,32 @@
 # is a string balanced
 # balanced -> {} squiggles , [] brackets, () parenthesis
+class Brackets:
 
+    def balanced_brackets(self,characters:str)->bool:
+        punctuation_stack=[]
+        # keys ---> (, {, [
+        # values -> ), }, ]
+        tokens={'(':')','{':'}','[':']'}
+        punctuations = ['[', ']', '(', ')', '{', '}']
+        for char in characters:
+            if char in punctuations:
+                punctuation_stack.append(char)
+            if len(punctuation_stack)>1 and punctuation_stack[-2] in tokens.keys():
+                if punctuation_stack[-1]==tokens[punctuation_stack[-2]]:
+                    punctuation_stack.remove(punctuation_stack[-1])
+                    punctuation_stack.remove(punctuation_stack[-1])
 
-def balanced_brackets(characters:str)->bool:
-    punctuation_stack=[]
-    # keys ---> (, {, [
-    # values -> ), }, ]
-    tokens={'(':')','{':'}','[':']'}
-    punctuations = ['[', ']', '(', ')', '{', '}']
-    for char in characters:
-        if char in punctuations:
-            punctuation_stack.append(char)
-        if len(punctuation_stack)>1 and punctuation_stack[-2] in tokens.keys():
-            if punctuation_stack[-1]==tokens[punctuation_stack[-2]]:
-                punctuation_stack.remove(punctuation_stack[-1])
-                punctuation_stack.remove(punctuation_stack[-1])
+        return True if len(punctuation_stack)==0 else False
 
-    return True if len(punctuation_stack)==0 else False
-
-
-
-print(balanced_brackets('(())'))
-
+if __name__ == '__main__':
+    test_obj=Brackets()
+    print(test_obj.balanced_brackets('('))
+    # [()]{}{[()()]()} -> True
+    print(test_obj.balanced_brackets('[()]{}{[()()]()}'))
+    # {{[[(())]]}}  -> True
+    print(test_obj.balanced_brackets('{{[[(())]]}}'))
+    # }([]]][[){}}[[)}[(}(}]{(}[{}][{}](}]}))]{][[}(({(]}[]{[{){{(}}[){[][{[]{[}}[)]}}]{}}(} NO
+    print(test_obj.balanced_brackets('}([]]][[){}}[[)}[(}(}]{(}[{}][{}](}]}))]{][[}(({(]}[]{[{){{(}}[){[][{[]{[}}[)]}}]{}}(}'))
 """
 ( -> ( 
 ([ -> ([
