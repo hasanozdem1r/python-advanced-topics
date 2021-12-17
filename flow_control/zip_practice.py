e52c9bc4b14a4caa6f188272aba80d259b261180
Hasan <hasanozdemir1@trakya.edu.tr>
2021-12-17 22:18:56 +0300

    Zip function practices

---
+++ flow_control/zip_practice.py
@@ -0,0 +1,19 @@
+"""
+Python script to understand fundamentals of ZIP function
+
+@ Hasan Özdemir 2021
+"""
+"""
+The zip() function returns a zip object, which is an iterator of tuples where the first item in each passed iterator is paired together, and then the second item in each passed iterator are paired together etc.
+
+If the passed iterators have different lengths, the iterator with the least items decides the length of the new iterator.
+
+"""
+from string import ascii_lowercase,ascii_uppercase
+
+letters_lower=list(ascii_lowercase)
+letters_upper=list(ascii_uppercase)
+print(letters_upper)
+for letters_lower,letters_upper in zip(letters_lower,letters_upper):
+    print(letters_lower,letters_upper)
+
