8956e05fa4f987ccdbbba929bab1d8a679c0e54c
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-17 20:09:50 +0300

    Metacharacters : * + ? {}

--- regular_expressions/regex_201.py
+++ regular_expressions/regex_201.py
@@ -11,7 +11,7 @@ name_list = ["özcan", "mehmet", "süleyman", "selim", "kemal", "özkan", "esra"
 
 # [] Brackets metacharacter : Refer set of characters
 # [chk]
-check_match = lambda item: re.search('öz[chk]an', item)
+check_match = lambda item: re.search('öz[chk]an', item) # must be one of the letter from the group (c,h,k)
 matches = [check_match(item).group() for item in name_list if check_match(item)]
 print(matches)
 
@@ -19,12 +19,46 @@ create_star()
 # [0-9][A-Z]
 numbers = ["23BH56", "TY76Z", "4Y7UZ", "TYUDZ", "34Y34", "12345"]
 for number in numbers:
-    if re.match('[0-9][A-Z]', number):
+    if re.match('[0-9][A-Z]', number): # there must be 1 number and 1 uppercase letter minimum
         print(number)
 
 create_star()
 # . Dot metacharacter : Refer any character except new line for on
 # e time occurrence
 for number in numbers:
-    if re.match('..Y', number):
+    if re.match('..Y', number): # we referred there must be exactly 2 letter before Y
         print(number)
+
+create_star()
+# * Star metacharacter : Refer item 0 or more occurrences
+list2=["st", "sat", "saat", "saaat", "falanca"]
+for i in list2:
+    if re.match('sa*t',i): # we referred a letter can repeat 0 or more times
+        print(i)
+
+create_star()
+# get all .py files from the current folder
+from os import listdir,getcwd
+index=listdir(getcwd())
+for i in index:
+    if re.match('.*py',i):
+        print(i)
+
+create_star()
+# + (plus) metacharacter : One or more occurrences
+name_list2=["ahmet", "mehmet", "met", "kezban"]
+for i in name_list2:
+    if re.match('.+met',i):
+        print(i)
+
+create_star()
+# ? (question mark) : Zero or one occurrence
+for i in list2:
+    if re.match('sa?t',i):
+        print(i)
+
+create_star()
+# {} (braces) : Exactly the specified number of occurrences
+for i in list2:
+    if re.match('sa{3}t',i):
+        print(i)
