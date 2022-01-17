178067aa099c8b9d3d4a4fd7b32c41cc6758be0c
Hasan <hasanozdemir1@trakya.edu.tr>
2022-01-17 18:35:22 +0300

    Intro to metacharacters. Dot and brackets usage in practice

--- regular_expressions/regex_201.py
+++ regular_expressions/regex_201.py
@@ -1,5 +1,30 @@
+"""
+This script is created to teach fundamental of metacharacters at RegEx
+@Hasan Ã–zdemir 01/17/2022
+"""
 import re
 
-name_list=["özcan", "mehmet", "süleyman", "selim","kemal", "özkan", "esra", "dündar", "esin","esma", "özhan", "özlem"]
+create_star = lambda: print("*" * 50)
 
-print(re.search('öz[chk]an',name_list))
+name_list = ["Ã¶zcan", "mehmet", "sÃ¼leyman", "selim", "kemal", "Ã¶zkan", "esra", "dÃ¼ndar", "esin", "esma", "Ã¶zhan",
+             "Ã¶zlem"]
+
+# [] Brackets metacharacter : Refer set of characters
+# [chk]
+check_match = lambda item: re.search('Ã¶z[chk]an', item)
+matches = [check_match(item).group() for item in name_list if check_match(item)]
+print(matches)
+
+create_star()
+# [0-9][A-Z]
+numbers = ["23BH56", "TY76Z", "4Y7UZ", "TYUDZ", "34Y34", "12345"]
+for number in numbers:
+    if re.match('[0-9][A-Z]', number):
+        print(number)
+
+create_star()
+# . Dot metacharacter : Refer any character except new line for on
+# e time occurrence
+for number in numbers:
+    if re.match('..Y', number):
+        print(number)
