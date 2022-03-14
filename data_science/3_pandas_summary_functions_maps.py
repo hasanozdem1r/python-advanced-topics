d31dac8e4dc75aa1f79b829822263465532aa2d7
Hasan <hasanozdemir1@trakya.edu.tr>
2022-03-15 00:31:01 +0300

    black, typing applied. General refactoring

--- data_science/3_pandas_summary_functions_maps.py
+++ data_science/3_pandas_summary_functions_maps.py
@@ -7,7 +7,7 @@ import pandas as pd
 draw_star = lambda: print(50 * "*")
 
 # Reading data from csv
-df_farmers = pd.read_csv('datasets/farmers_markets_from_usda.csv')
+df_farmers = pd.read_csv("datasets/farmers_markets_from_usda.csv")
 
 # Summary functions
 # Pandas provides many simple "summary functions" (not an official name) which restructure the data in some useful way.
@@ -36,6 +36,6 @@ one set of values and "maps" them to another set of values
 In data science we often have a need for creating new representations from existing data, 
 or for transforming data from the format it is in now to the format that we want it to be in later.
 """
-df_x_mean=df_farmers.x.mean()
-print(df_farmers.x.map(lambda p:p-df_x_mean))
+df_x_mean = df_farmers.x.mean()
+print(df_farmers.x.map(lambda p: p - df_x_mean))
 draw_star()
--- projects/twitter_trends/twitter_helpers.py
+++ projects/twitter_trends/twitter_helpers.py
@@ -10,10 +10,10 @@ def filter_by_country(country):
     :param country: <str> Name of country for top trends query
     :return: <str> query link to fetch data
     """
-    if country != '':
+    if country != "":
         country = country.lower()
         # update the query
-        initial_link = f'https://trends24.in//{country}/'
+        initial_link = f"https://trends24.in//{country}/"
         return initial_link
     else:
         # fetch worldwide top trends
@@ -45,9 +45,9 @@ def format_time(trend_time: str) -> str:
     """
     # datatime old format -> dd:mm:yyyy hh:mm:ss
     # datetime new format -> dd:mm:yyyy_hh:mm
-    trend_time = trend_time.replace(' ', '_')[:-3]
+    trend_time = trend_time.replace(" ", "_")[:-3]
     return trend_time
 
 
-if '__main__' == __name__:
-    print('Twitter Helpers Test')
+if "__main__" == __name__:
+    print("Twitter Helpers Test")
