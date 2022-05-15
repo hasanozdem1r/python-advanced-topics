34467a7f2c14a480e831dd29006d8f442ed1d16d
hasanozdem1r <hasanozdemir1@trakya.edu.tr>
2022-05-15 15:46:25 +0300

    Various topics implemented

---
+++ best_practices/return_none.py
@@ -0,0 +1,34 @@
+"""
+This script is aiming to show how to return nothing / None in Python
+@Hasan Ozdemir 05-13-2022
+"""
+# below all methods return the same thing
+def none_1() -> None:
+    name = "Hasan"
+    surname = "Ozdemir"
+
+
+def none_2() -> None:
+    name = "Hasan"
+    surname = "Ozdemir"
+    return None
+
+
+def none_3() -> None:
+    """Bare return statement implies `return None`"""
+    name = "Hasan"
+    surname = "Ozdemir"
+    return None
+
+
+def none_4(value) -> str:
+    """Missing return statement implies `return None`"""
+    if value:
+        return "x"
+
+
+"""
+All three functions properly return None 
+if you pass them a falsy value as the sole argument
+"""
+print(none_1(), none_2(), none_3(), none_4(value=""))
---
+++ best_practices/what_is_repr.py
@@ -0,0 +1,22 @@
+"""
+This script summarize repr method
+@Hasan Ozdemir 05-13-2022
+"""
+
+
+class Tractor:
+    def __init__(self, horse_power: int, model: int, cylinder: int, km: int):
+        self.horse_power = horse_power
+        self.model = model
+        self.cylinder = cylinder
+        self.km = km
+
+
+    def __str__(self):
+        return f"{self.horse_power} - {self.model} - {self.cylinder} - {self.km}"
+
+    def __repr__(self):
+        return f"{self.horse_power} - {self.model} - {self.cylinder} - {self.km}"
+if __name__=="__main__":
+    fiat_obj=Tractor(horse_power=64,model=1982,cylinder=3,km=1000)
+    print(fiat_obj)
