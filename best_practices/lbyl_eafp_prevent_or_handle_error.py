e3b5b32bbb14bdf51c8fa2c8905040654fb936e5
hasanozdem1r <hasanozdemir1@trakya.edu.tr>
2022-07-10 18:00:38 +0300

    explained

---
+++ best_practices/lbyl_eafp_prevent_or_handle_error.py
@@ -0,0 +1,17 @@
+"""
+This script cover basics of LBYL and EAFP
+@Hasan Özdemir 07-10-2022
+"""
+
+# LBYL -> look before you leap -> Preventing errors from occurring
+# EAFP -> easier to ask forgiveness than permission -> Handling errors after they occur
+
+"""
+You can either prevent errors before they happen or handle errors after they’ve happened
+    1.Prevent errors or exceptional situations from happening
+    2.Handle errors or exceptional situations after they happen
+
+Look before you leap. This coding style explicitly tests for pre-conditions before making calls or lookups. 
+This style contrasts with the EAFP approach and is characterized by the presence of many if statements.
+"""
+https://realpython.com/python-lbyl-vs-eafp/#:~:text=EAFP%2C%20or%20easier%20to%20ask,exception%20and%20handle%20it%20appropriately.
