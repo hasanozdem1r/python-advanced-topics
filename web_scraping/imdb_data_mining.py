e653bb8cd139411fb9a0b5a1877a77078b4a6366
Hasan Özdemir <hasanozdemir1@trakya.edu.tr>
2023-02-02 23:40:39 +0300

    chore: get_page_content methos is created to parse link html

--- web_scraping/imdb_data_mining.py
+++ web_scraping/imdb_data_mining.py
@@ -2,17 +2,27 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import os
+import asyncio
 
-PAGE = "https://www.imdb.com/chart/top/"
+
+def get_page_content(link: str):
+    """
+    Fetch parsed HTML for given link
+    :param link: website address
+    :return: parsed HTML
+    """
+    page_content = requests.get(link).content
+    soup_obj = BeautifulSoup(page_content, "html.parser")
+    return soup_obj
 
 
-def get_top_250(page_content) -> pd.DataFrame:
+def get_top_250(link: str = "https://www.imdb.com/chart/top/") -> pd.DataFrame:
     """
     Get IMDB top 250 movies and metadata
     :param page_content: bs4 object
-    :return: _description_
+    :return:
     """
-    soup_obj = BeautifulSoup(page_content, "html.parser")
+    soup_obj = get_page_content(link=link)
 
     movies = soup_obj.find("tbody", {
         "class": "lister-list"
@@ -45,5 +55,4 @@ def get_top_250(page_content) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    page_content = requests.get(PAGE).content
-    get_top_250(page_content=page_content)
+    get_top_250(link="https://www.imdb.com/chart/top/")
