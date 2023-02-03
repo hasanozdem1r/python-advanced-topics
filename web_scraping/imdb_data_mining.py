0491a05b387b0758a00b92fd5798a1b02b2d787b
Hasan Özdemir <hasanozdemir1@trakya.edu.tr>
2023-02-03 16:11:25 +0300

    chore: feature scrapping

--- web_scraping/imdb_data_mining.py
+++ web_scraping/imdb_data_mining.py
@@ -3,15 +3,21 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import asyncio
+from typing import List, Dict
 
 
-def get_page_content(link: str):
+def get_page_content(link: str, parser="html.parser"):
     """
     Fetch parsed HTML for given link
     :param link: website address
     :return: parsed HTML
     """
-    page_content = requests.get(link).content
+    # use fake User-Agent to deal 403 Forbidden
+    headers: Dict[str, str] = {
+        'User-Agent':
+            'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+    }
+    page_content = requests.get(link, headers=headers).content
     soup_obj = BeautifulSoup(page_content, "html.parser")
     return soup_obj
 
@@ -54,5 +60,25 @@ def get_top_250(link: str = "https://www.imdb.com/chart/top/") -> pd.DataFrame:
     print(movies_df)
 
 
+def get_movie_detail(links: List[str]):
+    movie_details: List = []
+    for link in links:
+        soup_obj = get_page_content(link=link)
+        #get description
+        description = soup_obj.find("span", {"class": "sc-16ede01-0"}).text
+        soup_obj = get_page_content(link=link, parser="lxml")
+        # get director with xpath
+        director = soup_obj.find(
+            xpath=
+            '//*[@id="__next"]/main/div/section[1]/div/section/div/div[1]/section[4]/ul/li[1]/div/ul/li/a'
+        ).text
+        print(director)
+        movie_details.append({
+            "link": link,
+            "description": description,
+        })
+
+
 if __name__ == "__main__":
-    get_top_250(link="https://www.imdb.com/chart/top/")
+    #get_top_250(link="https://www.imdb.com/chart/top/")
+    get_movie_detail(["https://www.imdb.com/title/tt0068646/"])
