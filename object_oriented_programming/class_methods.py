2e8e9f017b9026d796281dc23e3bfbab6779c70e
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2023-02-25 22:03:40 +0300

    fix: pre-commit formmatter changes

--- object_oriented_programming/class_methods.py
+++ object_oriented_programming/class_methods.py
@@ -4,31 +4,34 @@ An object is created using the constructor of the class. This object will then b
 Python has three different method types. static, class and instance method
 """
 
+
 # living creature
 class Organism:
 
     @staticmethod
     def who_am_i():
         return "I am an organism"
-    
+
     @classmethod
-    def who_you_are(cls,name:str):
+    def who_you_are(cls, name: str):
         return f"I am {name}"
-    
+
     def who_they_are(self):
         return "They are instance class and they can access all class methods"
+
 
 class Human:
     pass
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     # call static method
     print(Organism.who_am_i())
     # call class method (1)
     print(Organism.who_you_are(name="Hasan"))
     # call class method (2)
-    obj=Organism()
+    obj = Organism()
     print(obj.who_you_are(name="Hasan"))
     # call instance method
-    obj2=Organism
+    obj2 = Organism
     print(obj.who_they_are())
