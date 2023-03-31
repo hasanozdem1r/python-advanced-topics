ef3552c1ffcdf24625032e5198684e885362c3a5
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2023-03-31 17:58:05 +0300

    fix: pre-commit fix

--- data_science/load_data.py
+++ data_science/load_data.py
@@ -2,12 +2,19 @@ import os
 import csv
 from typing import List
 import pandas as pd
+from dask import dataframe as dd
+import polars as pl
 
-CURRENT_DIR=os.path.dirname(os.path.abspath(__file__))
-DATA_PATH=f'{CURRENT_DIR}/../data/traffic_crashes.csv'
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = f'{CURRENT_DIR}/../data/traffic_crashes.csv'
 
 
-def read_csv_1(file_path: str, chunk_size: int = None, mode: str = 'r', delimiter: str = ';', encoding: str = 'utf-8') -> List[List[str]]:
+# with csv core package
+def read_csv_1(file_path: str,
+               chunk_size: int = None,
+               mode: str = 'r',
+               delimiter: str = ';',
+               encoding: str = 'utf-8') -> List[List[str]]:
     """
     Reads a CSV file and returns its content as a list of lists, where each list corresponds to a row in the file.
 
@@ -21,17 +28,22 @@ def read_csv_1(file_path: str, chunk_size: int = None, mode: str = 'r', delimite
     rows = []
     with open(file_path, mode=mode, encoding=encoding, newline='') as file:
         reader = csv.reader(file, delimiter=delimiter)
-        if chunk_size is None:
-            rows = [row for row in reader]
-        else:
+        if chunk_size is not None:
             while True:
                 chunk = [row for i, row in enumerate(reader) if i < chunk_size]
                 if not chunk:
                     break
                 rows.extend(chunk)
+        else:
+            rows = [row for row in reader]
     return rows
 
-def read_csv_2(file_path: str, chunk_size: int = None,delimiter: str = ';', encoding: str = 'utf8')->pd.DataFrame:
+
+# with pandas
+def read_csv_2(file_path: str,
+               chunk_size: int = None,
+               delimiter: str = ';',
+               encoding: str = 'utf8') -> pd.DataFrame:
     """
     Read a CSV file into a Pandas DataFrame.
 
@@ -41,16 +53,55 @@ def read_csv_2(file_path: str, chunk_size: int = None,delimiter: str = ';', enco
     :param encoding: The encoding used to read the CSV file., defaults to 'utf8'
     :return: A Pandas DataFrame containing the data from the CSV file.
     """
-    if chunk_size != None:
-        chunks=[]
-        for chunk in pd.read_csv(filepath_or_buffer=file_path,chunksize=chunk_size,delimiter=delimiter,encoding=encoding):
+    if chunk_size is not None:
+        chunks = []
+        for chunk in pd.read_csv(filepath_or_buffer=file_path,
+                                 chunksize=chunk_size,
+                                 delimiter=delimiter,
+                                 encoding=encoding):
             chunks.append(chunk)
-        df=pd.concat(chunks)
-        return df
+        df = pd.concat(chunks)
     else:
-        df=pd.read_csv(filepath_or_buffer=file_path,chunksize=chunk_size,delimiter=delimiter,encoding=encoding)
-        return df
+        df = pd.read_csv(filepath_or_buffer=file_path,
+                         delimiter=delimiter,
+                         encoding=encoding)
+    return df
 
-if __name__=="__main__":
-    print(read_csv_1(file_path=DATA_PATH,chunk_size=1000))
-    print(read_csv_2(file_path=DATA_PATH,chunk_size=1000))
+
+# with dask
+def read_csv_3(file_path: str,
+               chunk_size: int = None,
+               delimiter: str = ';',
+               encoding: str = 'utf8'):
+    if chunk_size is not None:
+        df = dd.read_csv(file_path,
+                         chunksize=chunk_size,
+                         delimiter=delimiter,
+                         encoding=encoding)
+    else:
+        df = dd.read_csv(file_path, delimiter=delimiter, encoding=encoding)
+    return df
+
+
+# with polars
+def read_csv_4(file_path: str,
+               chunk_size: int = None,
+               delimiter: str = ';',
+               encoding: str = 'utf8'):
+    if chunk_size is not None:
+        chunks = []
+        for df in pl.read_csv(source=file_path,
+                              batch_size=chunk_size,
+                              separator=delimiter,
+                              encoding=encoding):
+            chunks.append(df)
+        return pl.concat(chunks)
+    else:
+        return pl.read_csv(file_path, delimiter=delimiter, encoding=encoding)
+
+
+if __name__ == "__main__":
+    read_csv_1(file_path=DATA_PATH, chunk_size=1000)
+    read_csv_2(file_path=DATA_PATH, chunk_size=1000)
+    read_csv_3(file_path=DATA_PATH, chunk_size=1000)
+    read_csv_4(file_path=DATA_PATH, chunk_size=1000)
