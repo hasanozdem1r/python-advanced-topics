e3787b252646ecb32fe23c416211e6a92e698716
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2023-04-09 03:05:36 +0300

    feature: metaclass approach is explained

---
+++ events/james_powell_pydata_2017/library2.py
@@ -0,0 +1,12 @@
+# how to enforce constraints from derived class to base class
+# 1. using via __built_class__ (least common approach)
+# 2. usinng metaclass
+class BaseMeta(type):
+    def __new__(cls,name,bases,body):
+        if not 'bar' in body:
+            raise TypeError('bad user class')
+        return super().__new__(cls,name,bases,body)
+
+class Base(metaclass=BaseMeta):
+    def foo(self):
+        return self.bar()
