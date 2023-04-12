107ff555ea34433b3aa8e8c4efb8ff5a78b02cc5
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2023-04-13 00:14:06 +0300

    higher order decorators example to run function ntimes

--- events/james_powell_pydata_2017/decorators.py
+++ events/james_powell_pydata_2017/decorators.py
@@ -1,30 +1,53 @@
 # intro to decorators
-# Decorators allow developers add extra ability to functions 
+# Decorators allow developers add extra ability to functions
 # without actually changing the function
 from time import time
 
+
 def timer(func):
-    def f(x,y):
-        b=time()
-        rv=func(x,y)
-        a=time()
+
+    def f(x, y):
+        b = time()
+        rv = func(x, y)
+        a = time()
         print(f'Elapsed:{a-b}')
         return rv
+
     return f
 
-@timer
-def add(x,y):
-    return x+y
+
+# higher order decorators
+def n_times(n):
+
+    def inner(f):
+
+        def wrapper(*args, **kwargs):
+            for _ in range(n):
+                print('Running {.__name__}'.format(f))
+                rv = f(*args, **kwargs)
+            return rv
+
+        return wrapper
+
+    return inner
+
 
 @timer
-def sub(x,y):
-    return x-y
+def add(x, y):
+    return x + y
 
-print('Addition',add(10,20))
-print('Substraction',sub(10,20))
 
+@n_times(3)
+def sub(x, y):
+    return x - y
+
+
+print('Addition', add(10, 20))
+print('Substraction', sub(10, 20))
 """
 How to call
 python -i decorators.py
 
 """
+
+# continue from 1.06
