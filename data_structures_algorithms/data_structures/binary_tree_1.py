d1e2046b05987cfdc863e770b31a533ba37be0b2
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2024-06-17 14:02:08 +0300

    fix: b-tree updated

--- data_structures_algorithms/data_structures/binary_tree_1.py
+++ data_structures_algorithms/data_structures/binary_tree_1.py
@@ -1,21 +1,33 @@
 class Node:
 
-    def __init__(self, left=None, right=None, value=None) -> None:
-        self.left = left
-        self.right = right
+    def __init__(self,value=None) -> None:
+        self.left = None
+        self.right = None
         self.value = value
+    
+    def __str__(self) -> str:
+        return str(self.value)
+
+    def __repr__(self) -> str:
+        return str(self.value)
+
+# types of traversal
+# 1. in-order -> left, root, right : [4, 2, 5, 1, 3]
+# 2. pre-order ->  root, left, right: [1, 2, 4, 5, 3]
+# 3. post-order -> left, right, root : [4, 5, 2, 3, 1]
+
+# in-order : 4 -> 2 -> 5 -> 1 - 3
 
 
-if __name__ == "_main__":
+if __name__ == "__main__":
     """
-            3
-        6       9
-    12
+            1
+        2           3
+    4       5
     """
-    n1 = Node(value=3)
-    n2 = Node(value=6)
-    n3 = Node(value=9)
-    n4 = Node(value=12)
-    n1.left = n2
-    n1.right = n3
-    n3.left = n4
+    root = Node(value=1)
+    root.left = Node(value=2)
+    root.right = Node(value=3)
+    root.left.left = Node(value=4)
+    root.left.right = Node(value=5)
+    print(root)
