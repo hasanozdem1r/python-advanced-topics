5d5ddbc558bba05c1df7dd1dce1176c4cd9f4d48
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2024-10-02 23:40:09 +0300

    fix: todo: remove later

---
+++ data_science/wap_pattern.py
@@ -0,0 +1,2 @@
+https://lakefs.io/blog/data-engineering-patterns-write-audit-publish/
+https://www.startdataengineering.com/post/implement_data_quality_with_great_expectations/
--- data_structures_algorithms/data_structures/binary_tree_1.py
+++ data_structures_algorithms/data_structures/binary_tree_1.py
@@ -1,9 +1,11 @@
+from typing import Optional,List
+
 class Node:
 
     def __init__(self,value=None) -> None:
-        self.left = None
-        self.right = None
-        self.value = value
+        self.left: Optional[Node] = None
+        self.right: Optional[Node] = None
+        self.value: int = value
     
     def __str__(self) -> str:
         return str(self.value)
@@ -16,9 +18,30 @@ class Node:
 # 2. pre-order ->  root, left, right: [1, 2, 4, 5, 3]
 # 3. post-order -> left, right, root : [4, 5, 2, 3, 1]
 
-# in-order : 4 -> 2 -> 5 -> 1 - 3
+def preorder_traversal(node: Optional[Node]) -> List[int]:
+    result = []
+    if node:
+        result.append(node.value)
+        result += preorder_traversal(node.left)
+        result += preorder_traversal(node.right)
+    return result
 
 
+def inorder_traversal(node: Optional[Node]) -> List[int]:
+    result = []
+    if node:
+        result += inorder_traversal(node.left)
+        result.append(node.value)
+        result += inorder_traversal(node.right)
+    return result
+
+def postorder_traversal(node: Optional[Node]) -> List[int]:
+    result = []
+    if node:
+        result += postorder_traversal(node.left)
+        result += postorder_traversal(node.right)
+        result.append(node.value)
+    return result
 if __name__ == "__main__":
     """
             1
