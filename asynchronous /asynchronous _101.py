3c5720dca40e2b676fe42d7593692fdecbd4f505
Hasan Ozdemir <hasanozdemir1@trakya.edu.tr>
2024-11-10 23:32:01 +0300

    fix: content is fixed, moved proper folder

--- asynchronous /asynchronous _101.py
+++ asynchronous /asynchronous _101.py
@@ -1,13 +1,48 @@
 import asyncio
+import time
 
 
-async def is_dividable_to_3(number: int):
-    total = sum([int(i) for i in str(number)])
-    if total % 3 == 0 and total >= 3:
-        return True
-    else:
-        return False
+# Section 1
+async def sum(x1: int, x2: int) -> None:
+    print(f"Sum of {x1} and {x2} is calculating...")
+    await asyncio.sleep(1)
+    print(f"Sum of {x1} and {x2} is {x1+x2}")
 
 
-if __name__ == '__main__':
-    print(asyncio.run(is_dividable_to_3(0)))
+# calling a async function -> running a coroutine way 1
+asyncio.run(sum(x1=14, x2=16))
+
+
+# Section 2
+async def say_message_after(delay: int, message: str) -> None:
+    await asyncio.sleep(delay)
+    print(message)
+
+
+async def call_say_hello():
+    print(f"started at {time.strftime('%X')}")
+    # calling a async function -> running a coroutine way 2
+    await say_message_after(1, "hello")
+    await say_message_after(1, "hola")
+    print(f"finished  at {time.strftime('%X')}")
+
+
+# call call_say_hello
+asyncio.run(call_say_hello())
+
+
+async def call_say_hello():
+    t1 = asyncio.create_task(say_message_after(1, "merhaba"))
+    t2 = asyncio.create_task(say_message_after(1, "Привет"))
+    print(f"started at {time.strftime('%X')}")
+
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await t1
+    await t2
+
+    print(f"finished at {time.strftime('%X')}")
+
+
+# call call_say_hello
+asyncio.run(call_say_hello())
